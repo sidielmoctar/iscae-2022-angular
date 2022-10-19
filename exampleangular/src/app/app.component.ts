874f7c56df4f9d615b552f7a4636ff12ed1f7d83
SIDI <a.sidielmoctar@gmail.com>
2022-10-19 10:27:56 +0000

    intégration datepicker

--- exampleangular/src/app/app.component.ts
+++ exampleangular/src/app/app.component.ts
@@ -10,37 +10,12 @@ import {FormControl} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  msg: IMessage | undefined;
-  nombrePopulation = 5246369852;
-  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
-  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
-  originally bred for hunting.`;
-
-  myControl = new FormControl();
-  options: string[] = ['One', 'Two', 'Three'];
-  filteredOptions: Observable<string[]> = EMPTY;
-
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter(option => option.toLowerCase().includes(filterValue));
-  }
-
 
   constructor(private messageService: MessageService) {
   }
 
   ngOnInit(): void {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value)),
-    );
-    console.log("111111111111");
-    this.messageService.getmessage().subscribe(message => {
-      console.log("222222222");
-      this.msg = message;
-    });
-    console.log("33333333333333");
+
     }
 
 
--- exampleangular/src/app/material/material.module.ts
+++ exampleangular/src/app/material/material.module.ts
@@ -1,5 +1,4 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
 import {MatCardModule} from "@angular/material/card";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
@@ -7,7 +6,8 @@ import {MatBadgeModule} from "@angular/material/badge";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatAutocompleteModule} from "@angular/material/autocomplete";
 import {MatInputModule} from "@angular/material/input";
-
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
 
 
 @NgModule({
@@ -18,7 +18,9 @@ import {MatInputModule} from "@angular/material/input";
     MatBadgeModule,
     MatFormFieldModule,
     MatAutocompleteModule,
-    MatInputModule
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ]
 })
 export class MaterialModule { }
